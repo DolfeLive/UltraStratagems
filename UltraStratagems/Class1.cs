008e356753c1973d1890e488010499b47e59107b
DolfeLive <dolfelive@gmail.com>
2025-02-17 14:56:51 +1100

    comnit

--- UltraStratagems/Class1.cs
+++ UltraStratagems/Class1.cs
@@ -15,6 +15,8 @@ global using UltraStratagems.Stratagems;
 global using static UltraStratagems.Class1;
 global using static UltraStratagems.AssetStuff;
 using UnityEngine.Assertions;
+using System.Collections;
+using System.Linq;
 
 namespace UltraStratagems;
 
@@ -129,34 +131,134 @@ public partial class Class1 : BaseUnityPlugin
             }
         }    
 
-        if (DeathRay != null)// && Input.GetKeyDown(KeyCode.P))
-        {
-            Ray ray = new Ray(nm.cc.transform.position, nm.cc.transform.forward);
-            if (Physics.Raycast(ray, out RaycastHit hitInfo, 100f, LayerMask.GetMask("Environment", "Outdoors", "EnemyTrigger")))
-            {
-                Vector3 direction = (hitInfo.point - DeathRay.transform.position).normalized;
-                Quaternion newRot = Quaternion.LookRotation(direction, DeathRay.transform.up);
-                print($"Ray hit: {hitInfo.collider}, pos: {hitInfo.point}, {hitInfo.collider.name}, lay: {hitInfo.collider.gameObject.layer}");
-                 
-                print($"NewRot: {newRot}");
-                DeathRay.transform.rotation = newRot;
-            } 
-            else
-            {
-                print("Hit nothin");
-            }
-        }
-
-        DeathRay.GetComponent<ContinuousBeam>().canHitEnemy = false;
-        DeathRay.GetComponent<ContinuousBeam>().canHitPlayer = false;
         
-        if (DeathRay != null && Input.GetKeyDown(KeyCode.P))
+
+        if (DeathRay != null)
         {
-            DeathRay.GetComponent<ContinuousBeam>().canHitEnemy = true;
-            DeathRay.GetComponent<ContinuousBeam>().canHitPlayer = true;
+            DeathRay.GetComponent<ContinuousBeam>().canHitEnemy = false;
+            DeathRay.GetComponent<ContinuousBeam>().canHitPlayer = false;
 
+            if (Input.GetKeyDown(KeyCode.P))
+            {
+                DeathRayAiming = true;
+                StartCoroutine(DeathRayAimSequence()); 
 
+            }
+
+            
         }
+
     }
 
+    bool DeathRayAiming = false;
+
+    IEnumerator DeathRayAimSequence()
+    {
+        //DeathRay.GetComponent<ContinuousBeam>().canHitEnemy = true;
+        //DeathRay.GetComponent<ContinuousBeam>().canHitPlayer = true;
+        Vector3 targetPoint = Vector3.zero;
+        float timeTillShoot = 1f;
+        float radius = 50f;
+        EnemyIdentifier Target = null!;
+
+        try
+        {
+            Ray ray = new Ray(nm.cc.transform.position, nm.cc.transform.forward);
+            if (Physics.Raycast(ray, out RaycastHit hitInfo, 100f, LayerMask.GetMask("Environment", "Outdoors", "Default")))
+            {
+                targetPoint = hitInfo.point;
+            }
+            RaycastHit[] hits = Physics.SphereCastAll(new Ray(targetPoint, Vector3.up), radius, radius, LayerMask.GetMask("EnemyTrigger"));
+            if (hits.Length < 1)
+            {
+                Debug.LogWarning("Spherecast got nothin");
+                yield break;
+            }
+            List<EnemyIdentifier> identifiers = hits.Select(_ => _.collider.gameObject.GetComponent<EnemyIdentifier>()).ToList();
+            print($"Targets in area: {identifiers.Count}");
+            Target = identifiers[0];
+
+            foreach (var item in identifiers)
+            {
+                if (EnemyStrengthRanks[item.enemyType] > EnemyStrengthRanks[item.enemyType])
+                {
+                    Target = item;
+                }
+            }
+
+        }
+        catch (NullReferenceException e)
+        {
+            Debug.LogError($"Null ref: {e.Message}");
+            DeathRayAiming = false;
+        }
+
+
+        while (DeathRayAiming)
+        {
+            timeTillShoot += Time.deltaTime;
+            
+            DeathRayRotateTowards(Target.transform.position, 5f);
+
+            if (timeTillShoot > 1)
+                DeathRayAiming = false;
+
+            yield return null;
+        }
+        DeathRay.GetComponent<ContinuousBeam>().canHitEnemy = true;
+        DeathRay.GetComponent<ContinuousBeam>().canHitPlayer = true;
+
+    }
+
+    void DeathRayRotateTowards(Vector3 Point, float t)
+    {
+        Vector3 direction = (Point - DeathRay.transform.position).normalized;
+        Quaternion newRot = Quaternion.LookRotation(direction, DeathRay.transform.up);
+
+        print($"Rotating Death rat: {newRot}");
+
+        DeathRay.transform.rotation = Quaternion.Slerp(DeathRay.transform.rotation, newRot, speed * Time.deltaTime);
+    }
+
+    public static Dictionary<EnemyType, int> EnemyStrengthRanks = new Dictionary<EnemyType, int>
+    {
+        { EnemyType.Filth, 1 },
+        { EnemyType.Stray, 2 },
+        { EnemyType.Schism, 3 },
+        { EnemyType.Drone, 4 },
+        { EnemyType.Turret, 5 },
+        { EnemyType.MaliciousFace, 6 },
+        { EnemyType.Streetcleaner, 7 },
+        { EnemyType.Soldier, 8 },
+        { EnemyType.Cerberus, 9 },
+        { EnemyType.Swordsmachine, 10 },
+        { EnemyType.Stalker, 11 },
+        { EnemyType.HideousMass, 12 },
+        { EnemyType.Mindflayer, 13 },
+        { EnemyType.Virtue, 14 },
+        { EnemyType.CancerousRodent, 15 },
+        { EnemyType.VeryCancerousRodent, 16 },
+        { EnemyType.Idol, 17 },
+        { EnemyType.Mannequin, 18 },
+        { EnemyType.Mandalore, 19 },
+        { EnemyType.Gutterman, 20 },
+        { EnemyType.Guttertank, 21 },
+        { EnemyType.Minotaur, 22 },
+        { EnemyType.Centaur, 23 },
+        { EnemyType.Puppet, 24 },
+        { EnemyType.BigJohnator, 25 },
+        { EnemyType.Wicked, 26 },
+        { EnemyType.Ferryman, 27 },
+        { EnemyType.Leviathan, 28 },
+        { EnemyType.FleshPanopticon, 29 },
+        { EnemyType.FleshPrison, 30 },
+        { EnemyType.V2, 31 },
+        { EnemyType.V2Second, 32 },
+        { EnemyType.Gabriel, 33 },
+        { EnemyType.GabrielSecond, 34 },
+        { EnemyType.Minos, 35 },
+        { EnemyType.MinosPrime, 36 },
+        { EnemyType.Sisyphus, 37 },
+        { EnemyType.SisyphusPrime, 38 }
+    };
 }
